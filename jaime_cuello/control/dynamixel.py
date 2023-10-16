60c15b1085f37889ccc8d54f5ef5d94aa3262b6f
Fersanmartin <123670926+Fersanmartin@users.noreply.github.com>
2023-10-16 15:25:51 -0400

    Add files via upload

--- jaime_cuello/control/dynamixel.py
+++ jaime_cuello/control/dynamixel.py
@@ -6,30 +6,51 @@ import time
 import sys
 import rospy
 from dynamixel_workbench_msgs.srv import *
+from dynamixel_workbench_msgs.msg import *
 from std_msgs.msg import Float64MultiArray
 
+last_received_time = None
 class IMU():
     def __init__(self):
         self.IMU=list()
-        self.suscriber=rospy.Subscriber("gyro", Float64MultiArray, self.callback)
         
+        self.x=0
+        self.y=0
         
     def callback(self, data):
-
-        rospy.loginfo(data.data)
-        x, y ,z= 0, data.data[0], data.data[1]
-        print("Received x={}, y={}".format(x, y))
-        x = (x+90)*(310-50)/180 + 50
-        y = (y+90)*(800-260)/180 + 260
-        z = (z+90)*(800-260)/180 + 260
+        self.x, self.y =data.data[0], data.data[1]
+        #self.x = (self.x+90)*(800-260)/180 + 260
+        #self.y = (self.y+90)*(800-260)/180 + 260
+        
+       
         # Llama a la función goal_position para mover el servo
 
         #tercer motor
-        goal_position(3, int(x), min=50, max=310)
+        #goal_position(3, int(x), min=50, max=310)
         #Cuarto motor
-        goal_position(4, int(y), min=260, max=800)
+        #goal_position(4, int(y), min=260, max=800)
         #Quinto motor
-        goal_position(5, int(z), min=260, max=800)
+        #goal_position(5, int(z), min=260, max=800)
+
+    
+    def callback_vel(self,data):
+        global last_received_time
+        current_time = rospy.Time.now()
+    
+        if last_received_time is None or (current_time - last_received_time).to_sec() >= 0.05:
+            Position=data.dynamixel_state[0].present_position
+            last_received_time = current_time
+
+            print(Position)
+            if self.x<0:
+                v=(-1*self.x)*(1023/90) + 1024
+            else:
+                v=(1*self.x)*(1023/90)
+            goal_vel(5, v,Position,min=200, max=800)
+            
+        
+
+        
 
     def listener(self):
 
@@ -39,11 +60,15 @@ class IMU():
         # name for our 'listener' node so that multiple listeners can
         # run simultaneously.
         rospy.init_node('Dynamixel', anonymous=True)
-
+        
         rospy.Subscriber("gyro", Float64MultiArray, self.callback)
-
-        # spin() simply keeps python from exiting until this node is stopped
-        rospy.spin()
+        
+        rospy.Subscriber("/dynamixel_workbench/dynamixel_state", DynamixelStateList, self.callback_vel)
+        
+        
+        
+        rospy.spin() #simply keeps python from exiting until this node is stopped
+        
 
 
 def goal_position(ID , pos , min=0,max=1023 ):
@@ -68,6 +93,26 @@ def goal_position(ID , pos , min=0,max=1023 ):
         print("Service call failed: %s"%e)
 
 
+def goal_vel(ID , vel, Pos, min=0,max=1023):
+    #rospy.wait_for_service('/dynamixel_command')
+
+    if Pos<min and vel>1024:
+        vel=0
+
+    elif Pos>max and vel<1024:
+        vel=0
+    
+
+
+    try:
+        #Se manda el servicio para mover el motor
+        dynamixel_command = rospy.ServiceProxy('/dynamixel_workbench/dynamixel_command' , DynamixelCommand)
+        print ("ServiceProxy success ...")
+            
+        resp= dynamixel_command( '',ID,'Moving_Speed',vel)
+        return resp
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
 
 if __name__ == "__main__":
     # Inicializa la clase IMU
