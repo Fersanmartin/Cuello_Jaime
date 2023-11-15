ee42b5de9dc196d33927c46350a02887177305bf
Fersanmartin <123670926+Fersanmartin@users.noreply.github.com>
2023-11-15 14:08:15 -0500

    Add files via upload

--- jaime_cuello/control/dynamixel.py
+++ jaime_cuello/control/dynamixel.py
@@ -10,6 +10,7 @@ from dynamixel_workbench_msgs.msg import *
 from std_msgs.msg import Float64MultiArray
 
 
+
 matriz=[[[0000,0,0],[1000,0,0],[2000,0,0],[3000,0,0],[4000,0,0],[5000,0,0],[6000,0,0]],[[0000,0,0],[1000,0,0],[2000,0,0],[3000,0,0],[4000,0,0],[5000,0,0],[6000,0,0]]]
 
 last_received_time = None
@@ -19,10 +20,13 @@ class IMU():
         
         self.x=0
         self.y=0
-    
+        self.homie=[0,0,0,0,0]
+
+        self.pos=[0,0,0,0,0]
+        self.inicio=0
 
     #Callback para arduino serial
-    def callback(self, data):
+    def callback_IMU(self, data):
         self.x, self.y =data.data[0], data.data[1]
 
 
@@ -40,33 +44,53 @@ class IMU():
                new_speed=(abs(new_speed/2))+ 1024 
         else:
               new_speed=(abs(new_speed/2))
-        print(new_speed)
+        #print(new_speed)
         
 
-        goal_vel(4, new_speed,self.x,min=200, max=800)
+        goal_vel(4, new_speed)
 
 
     def callback_multivuelta(self,data):
+
         m=matriz[int(data.data[0])][int(data.data[1])]
         
         goal_position(1,m[0],min=-12000, max=1500)
         goal_position(2,m[1],min=100, max=4000)
 
         
+    #Función calback lectura posicion inidical
+    def callback_read(self,data):
+        
+        for i in range(5):
+            self.pos[i]=(data.dynamixel_state[i].present_position)
 
-    def listener(self):
+        if self.inicio==0:
+            self.homie=self.pos
+            self.inicio=1
+            print(self.homie)
+        
 
+
+
+    def Main(self):
+
+        #Se inicia nodo 
         rospy.init_node('Dynamixel', anonymous=True)
+
+        rospy.Subscriber("/dynamixel_workbench/dynamixel_state", DynamixelStateList, self.callback_read)
+
+        rospy.Subscriber("gyro", Float64MultiArray, self.callback_IMU)
         
-        #rospy.Subscriber("gyro", Float64MultiArray, self.callback)
-        
-        #rospy.Subscriber("/dynamixel_workbench/dynamixel_state", DynamixelStateList, self.callback_vel)
+        rospy.Subscriber("/dynamixel_workbench/dynamixel_state", DynamixelStateList, self.callback_vel)
         
         rospy.Subscriber("IK_node", Float64MultiArray, self.callback_multivuelta)
         
         rospy.spin() 
         
 
+
+
+###################################################################3
 
 def goal_position(ID , pos , min=0,max=1023 ):
     #rospy.wait_for_service('/dynamixel_command')
@@ -90,29 +114,38 @@ def goal_position(ID , pos , min=0,max=1023 ):
         print("Service call failed: %s"%e)
 
 
-def goal_vel(ID , vel, Pos, min=0,max=1023):
-    #rospy.wait_for_service('/dynamixel_command')
-
-
-    
-
-
+def goal_vel(ID , vel):
     try:
         #Se manda el servicio para mover el motor
         dynamixel_command = rospy.ServiceProxy('/dynamixel_workbench/dynamixel_command' , DynamixelCommand)
-        print ("ServiceProxy success ...")
+        #print ("ServiceProxy success ...")
             
         resp= dynamixel_command( '',ID,'Moving_Speed',vel)
         return resp
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
 
+
+
+#####################################################################################
+
+
+
+
+
+
+############################################################################################
+
+
+
+
+
 if __name__ == "__main__":
     # Inicializa la clase IMU
     imu = IMU()
     
     # Llama a la función listener para iniciar la suscripción al tópico "gyroscope"
-    imu.listener()
+    imu.Main()
 
         
         
